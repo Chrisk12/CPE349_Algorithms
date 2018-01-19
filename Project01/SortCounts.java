c7b935aee414b7f49058b37f27681aa919ad7c0f
Chris Kim <christopher.cuong.kim@gmail.com>
2018-01-19 10:09:47 -0800

    Added Part 3.

---
+++ Project01/SortCounts.java
@@ -0,0 +1,71 @@
+/*
+* @Author: Chris Kim, Thinh Luu
+* @Usernames: ckim65, tpluu
+* @Date:   2018-01-17 01:44:58
+* @Last Modified by:   Chris Kim
+* @Last Modified time: 2018-01-19 09:59:49
+*/
+
+import java.util.*;
+
+public class SortCounts {
+    
+    public static void main(String[] args) {
+        
+        long startTime;
+        long endTime;
+
+        long time1;
+        long time2;
+        long time3;
+
+        Random ran = new Random();
+        Sorts1 sort = new Sorts1();
+
+        // generates different sizes for each array
+        for(int size = 100; size <= 12800; size*=2) {
+
+        	long avgSS = 0;
+        	long avgMS = 0;
+        	long avgQS = 0;
+
+            // repeating process 100 times for each array size
+            for(int i = 0; i < 100; i++) {
+
+                // initializing and copying 3 arrays for each sort
+                int[] array1 = new int[12800];
+                
+                for(int j = 0; j < array1.length; j++) {
+
+                    array1[j] = ran.nextInt(500);
+                }
+
+                int[] array2 = new int[12800];
+                int[] array3 = new int[12800];
+
+                System.arraycopy(array1, 0 , array2, 0, array1.length);
+                System.arraycopy(array1, 0 , array3, 0, array1.length);
+
+                // computing for selection sort
+                avgSS += sort.selectionSort(array1, size);
+
+                // computing for merge sort
+                avgMS += sort.mergeSort(array2, size);
+
+                // computing for quicks sort
+                avgQS += sort.quickSort(array3, size);
+
+
+
+                
+            }
+)            displayResults(size, avgSS/100, avgMS/100, avgQS/100);
+        }
+    }
+
+    // displays the number of elements and the times for each sort
+    private static void displayResults(int N, long avgSS, long avgMS, long avgQS) {
+
+        System.out.println("N="+N+": C_ss="+avgSS+", C_ms="+avgMS+", C_qs="+avgQS);
+    }
+}
--- Project01/SortTimes.java
+++ Project01/SortTimes.java
@@ -1,64 +1,76 @@
-//
-//
-//
+/*
+* @Author: Chris Kim, Thinh Luu
+* @Usernames: ckim65, tpluu
+* @Date:   2018-01-10 09:16:14
+* @Last Modified by:   Chris Kim
+* @Last Modified time: 2018-01-19 10:09:08
+*/
+
 import java.util.*;
 
 public class SortTimes {
-   
-   public static void main(String[] args) {
-      
-      long startTime;
-      long endTime;
+    
+    public static void main(String[] args) {
+        
+        long startTime;
+        long endTime;
 
-      long time1;
-      long time2;
-      long time3;
+        long time1;
+        long time2;
+        long time3;
 
-      Random ran = new Random();
-      Sorts sort = new Sorts();
+        Random ran = new Random();
+        Sorts sort = new Sorts();
 
-      startTime = System.nanoTime() / 1000000;
+        startTime = System.nanoTime() / 1000000;
 
-      // generates different sizes for each array
-      for(int size = 5000; size <= 16000; size*=2) {
+        // generates different sizes for each array
+        for(int size = 5000; size <= 160000; size*=2) {
 
-         // repeating prpcess 5 times for each array size
-         for(int i = 0; i < 5; i++) {
+            // repeating prpcess 5 times for each array size
+            for(int i = 0; i < 5; i++) {
 
-            // initializing and copying 3 arrays for each sort
-            int[] array1 = new int[160000];
-            
-            for(int j = 0; j < array1.length; j++) {
+                // initializing and copying 3 arrays for each sort
+                int[] array1 = new int[160000];
+                
+                for(int j = 0; j < array1.length; j++) {
 
-               array1[j] = ran.nextInt(500);
+                    array1[j] = ran.nextInt(500);
+                }
+
+                int[] array2 = new int[160000];
+                int[] array3 = new int[160000];
+
+                System.arraycopy(array1, 0 , array2, 0, array1.length);
+                System.arraycopy(array1, 0 , array3, 0, array1.length);
+
+                // computing time for selection sort
+                startTime = System.nanoTime() / 1000000;
+                sort.selectionSort(array1, size);
+                endTime = System.nanoTime() / 1000000;
+                time1 = endTime - startTime;
+
+                // computing time for merge sort
+                startTime = System.nanoTime() / 1000000;
+                sort.mergeSort(array2, size);
+                endTime = System.nanoTime() / 1000000;
+                time2 = endTime - startTime;
+
+                // computing time for quicks sort
+                startTime = System.nanoTime() / 1000000;
+                sort.quickSort(array3, size);
+                endTime = System.nanoTime() / 1000000;
+                time3 = endTime - startTime;
+
+                displayResults(size, time1, time2, time3);
             }
+            System.out.println();
+        }
+    }
 
-            int[] array2 = array1;
-            int[] array3 = array1;
+    // displays the number of elements and the times for each sort
+    private static void displayResults(int N, long time1, long time2, long time3) {
 
-            // computing time for selection sort
-            sort.selectionSort(array1, size);
-            endTime = System.nanoTime() / 1000000;
-            time1 = endTime - startTime;
-
-            // computing time for merge sort
-            sort.mergeSort(array2, size);
-            endTime = System.nanoTime() / 1000000;
-            time2 = endTime - startTime;
-
-            // computing time for quicks sort
-            sort.quickSort(array3, size);
-            endTime = System.nanoTime() / 1000000;
-            time3 = endTime - startTime;
-
-            displayResults(size, time1, time2, time3);
-         }
-      }
-   }
-
-   // displays the number of elements and the times for each sort
-   private static void displayResults(int N, long time1, long time2, long time3) {
-
-      System.out.println("N="+N+": T_ss="+time1+", T_ms="+time2+"), T_qs="+time3);
-   }
+        System.out.println("N="+N+": T_ss="+time1+", T_ms="+time2+", T_qs="+time3);
+    }
 }
